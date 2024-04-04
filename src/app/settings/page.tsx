28378ab1ec2a4c65f2e9373a99a9b7b2ed5b5d02
AlinaSyniavska <alina22syniavska@gmail.com>
2024-04-04 13:07:48 +0300

    Created the navigation panel

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,7 @@
+export default function  Settings() {
+  return (
+    <main className={''}>
+      <div>Settings</div>
+    </main>
+  );
+}
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,16 +1,41 @@
 import React, { FC } from 'react';
-
+import Link from 'next/link';
+import { ClipboardList, Clock, LayoutDashboard, Settings } from 'lucide-react';
+import { usePathname } from 'next/navigation';
 
 interface IProps {}
 
 const Navbar: FC<IProps> = () => {
+  const currentPath = usePathname();
+
   return (
-    <>
+    <section>
+      <div className={'flex flex-col items-center justify-center gap-y-3'}>
+        <Link href="/" className={`${currentPath === '/' ? 'text-white' : ''}`}>
+          <div className={'p-3 hover:rounded-full hover:bg-white/25 hover:text-white'}>
+            <LayoutDashboard className={'h-5 w-5'} />
+          </div>
+        </Link>
 
+        <Link href="/efforts" className={`block ${currentPath === '/efforts' ? 'text-white' : ''}`}>
+          <div className={'p-3 hover:rounded-full hover:bg-white/25 hover:text-white'}>
+            <Clock className={'h-5 w-5'} />
+          </div>
+        </Link>
 
+        <Link href="/tasks" className={`${currentPath === '/tasks' ? 'text-white' : ''}`}>
+          <div className={'p-3 hover:rounded-full hover:bg-white/25 hover:text-white'}>
+            <ClipboardList className={'h-5 w-5'} />
+          </div>
+        </Link>
 
-
-    </>
+        <Link href="/settings" className={`${currentPath === '/settings' ? 'text-white' : ''}`}>
+          <div className={'p-3 hover:rounded-full hover:bg-white/25 hover:text-white'}>
+            <Settings className={'h-5 w-5'} />
+          </div>
+        </Link>
+      </div>
+    </section>
   );
 };
 
